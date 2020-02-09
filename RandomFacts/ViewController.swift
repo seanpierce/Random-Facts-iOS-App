55216da2f8384c286d2f8fe6232bc1fe54a5be93
Sean Pierce <sumler.sean@gmail.com>
2020-02-08 18:01:48 -0800

    Initial Commit

---
+++ RandomFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomFacts
+//
+//  Created by Sean Pierce on 2/8/20.
+//  Copyright © 2020 Sean Pierce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
