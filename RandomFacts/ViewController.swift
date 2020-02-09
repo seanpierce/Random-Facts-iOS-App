479f9905717e2bedbb0ba887011184989c107f6b
Sean Pierce <sumler.sean@gmail.com>
2020-02-09 07:01:03 -0800

    Add random color generation function

--- RandomFacts/ViewController.swift
+++ RandomFacts/ViewController.swift
@@ -22,6 +22,14 @@ class ViewController: UIViewController {
         "On average, it takes 66 days to form a new habit",
         "Mammoths still walked the Earth when the Great Pyramid was being built"
     ]
+    
+    let colors: Array<UIColor> = [
+        UIColor(red: 90/255, green: 281/255, blue: 181/255, alpha: 1),  // teal
+        UIColor(red: 222/255, green: 171/255, blue: 66/255, alpha: 1),  // yellow
+        UIColor(red: 253/255, green: 86/255, blue: 94/255, alpha: 1),   // red
+        UIColor(red: 239/255, green: 130/255, blue: 100/255, alpha: 1), // dark
+        UIColor(red: 85/255, green: 176/255, blue: 112/255, alpha: 1)   // green
+    ]
 
     @IBOutlet weak var randomFactLabel: UILabel!
     
@@ -32,12 +40,17 @@ class ViewController: UIViewController {
 
     @IBAction func showAnotherFunFact() {
         getRandomFact()
+        updateColor()
     }
     
     func getRandomFact() {
-        let randomIndex: Int = Int.random(in: 1 ... facts.count) - 1
+        let randomIndex: Int = Int.random(in: 0 ... facts.count - 1)
         randomFactLabel.text = facts[randomIndex]
     }
-
+    
+    func updateColor() {
+        let randomIndex: Int = Int.random(in: 0 ... colors.count - 1)
+        view.backgroundColor = colors[randomIndex]
+    }
 }
 
