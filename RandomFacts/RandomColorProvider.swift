1b3331bb31db582eff84d49f617207f4f27dc56b
Sean Pierce <sumler.sean@gmail.com>
2020-02-09 07:25:53 -0800

    Create random color provider class with data and get function

--- RandomFacts/RandomColorProvider.swift
+++ RandomFacts/RandomColorProvider.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2020 Sean Pierce. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct RandomColorProvider {
+    func get() -> UIColor {
+        return colors[Int.random(in: 0 ... colors.count - 1)]
+    }
+    
+    private let colors: Array<UIColor> = [
+        UIColor(red: 90/255, green: 281/255, blue: 181/255, alpha: 1),  // teal
+        UIColor(red: 222/255, green: 171/255, blue: 66/255, alpha: 1),  // yellow
+        UIColor(red: 253/255, green: 86/255, blue: 94/255, alpha: 1),   // red
+        UIColor(red: 239/255, green: 130/255, blue: 100/255, alpha: 1), // dark
+        UIColor(red: 85/255, green: 176/255, blue: 112/255, alpha: 1)   // green
+    ]
+}
