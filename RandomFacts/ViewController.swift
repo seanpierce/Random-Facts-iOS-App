a10c5525073db1cc4a3998eae5c49ee521a11ed2
Sean Pierce <sumler.sean@gmail.com>
2020-02-08 20:52:39 -0800

    Add data array and link elements to script

--- RandomFacts/ViewController.swift
+++ RandomFacts/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let facts: Array<String> = [
+        "Ostriches can run faster than horses",
+        "Ants stretch when they wake up in the morning",
+        "Olympic gold medals are actually made mostly of silver",
+        "You are born with 300 bones; by the time you are an adult you will have 206",
+        "It takes about 8 minutes for the light from the Sun to reach Earth",
+        "Some bamboo plants can grow almost a meter in just one day",
+        "The state of Florida is bigger than England",
+        "Some penguins can leap 2-3 meters out of the water",
+        "On average, it takes 66 days to form a new habit",
+        "Mammoths still walked the Earth when the Great Pyramid was being built"
+    ]
+
+    @IBOutlet weak var randomFactLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getRandomFact()
     }
 
+    @IBAction func showAnotherFunFact() {
+        getRandomFact()
+    }
+    
+    func getRandomFact() {
+        let randomIndex: Int = Int.random(in: 1 ... facts.count) - 1
+        randomFactLabel.text = facts[randomIndex]
+    }
 
 }
 
